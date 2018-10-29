e0324a2534ebd8cc0fe138eb6228764183d6a96b
Ismael Pérez Martín <ismaelprzm@gmail.com>
2018-10-29 21:45:16 +0100

    Añado las entities en la interfaz remota del bean GestionarUsuarios y añado las session bean de las entities de la interfaz en la capa de persistencia del bean GestionarUsuarios

---
+++ Practica1/GestionUsuariosInterface/src/Persistencia/AbonadoFacadeRemote.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persistencia;
+
+import Dominio.Abonado;
+import java.util.List;
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author Ismael Perez
+ */
+@Remote
+public interface AbonadoFacadeRemote {
+
+    void create(Abonado abonado);
+
+    void edit(Abonado abonado);
+
+    void remove(Abonado abonado);
+
+    Abonado find(Object id);
+
+    List<Abonado> findAll();
+
+    List<Abonado> findRange(int[] range);
+
+    int count();
+    
+}
