ba70fa43a43f115ccc2e3d018cbc65e67ecc7667
Rojo97 <victorrojo1997@gmail.com>
2018-12-12 18:16:09 +0100

    Creacion de dominio y entities

---
+++ Practica2/Rest/src/java/Persistencia/PreferenciaFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Persistencia;
+
+import Dominio.Preferencia;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author rojo
+ */
+@Stateless
+public class PreferenciaFacade extends AbstractFacade<Preferencia> {
+
+    @PersistenceContext(unitName = "RestPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public PreferenciaFacade() {
+        super(Preferencia.class);
+    }
+    
+}
