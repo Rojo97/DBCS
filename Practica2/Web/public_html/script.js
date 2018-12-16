0fcbf0984d6664b76f43165a92267ba1b4f1b5ac
Rojo97 <victorrojo1997@gmail.com>
2018-12-16 22:54:21 +0100

    Creación del cliente y primera vista

---
+++ Practica2/Web/public_html/script.js
@@ -0,0 +1,15 @@
+angular.module("practicaApp", [])
+        .constant("baseUrl", "http://localhost:8080/ServicioVino2/webresources/paraAngular")
+        .controller("appCtrl", function ($scope, $http, baseUrl) { // Inyectamos recursos
+            $scope.estado = "login";
+
+            $scope.loginAbonado = function (id) {
+                $scope.estado = "abonado";
+                console.log("abonado");
+            }
+
+            $scope.loginEmpleado = function (id) {
+                $scope.estado = "empleado";
+                console.log("empleado");
+            }
+        });
